baa029605c09186c74e6846ad14aafc091264b74
Alexander Tim Hobelsberger <alexander_tim.hobelsberger@student.reutlingen-university.de>
2022-04-25 13:52:15 +0200

    docker

---
+++ cloudcomputing/Routes/LoginRoute.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const path = require('path')
+const router = express.Router();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const User = require('../Models/user');
+const index = require('../index');
+
+const signInCounter = index.signInCounter;
+
+/**
+ * Takes post request on path /logIn
+ *
+ * Reads the attributes username and password from the Client. Than search for the User in local DB by using MongoDB's findOne() Method.
+ * If the username provided by Client can't be found in DB, it will return a 401 Status Code to the Client.
+ * If the username was found the given Password from Client will be hashed by SHA512 algorithm so it's no plain Text anymore. Thats how Passwords are Stored in DB.
+ * Also, by using the findOne() Method, the function is looking for the same Hash Value. If the User can't be found it will send a 401 StatusCode.
+ * If the Clients password and username is in DB, the function returns a 200 Status Code to Client.
+ * 
+ * @param {object}   req         The request Object from Client
+ * @param {object}   res         The Response Object from Server
+ *
+ * @return {number} Status Code of 401 by invalid User or Password
+ * @return {number} Status Code of 200 by valid User and Password
+ */
+router.post('/', async (req, res) => {
+    const username = req.body.username
+    const passwordFromClient = req.body.password
+    const user = await User.findOne({ username }).lean()//.lean gibt nur json zurück
+    if(!user){
+        console.log("Invalid User");
+        return res.sendStatus(401);
+    }
+    const passwordHashed = crypto.createHash("SHA512").update(passwordFromClient).digest();
+    const password = passwordHashed.toString('hex');
+    const passwordInDb = await User.findOne({ password }).lean()//.lean gibt nur json zurück
+    if(!passwordInDb){
+        console.log("Invalid Password");
+        return res.sendStatus(401);
+    }
+    if(user && passwordInDb){
+
+        return res.sendStatus(200);
+    }
+})
+
+module.exports = router;
---
+++ cloudcomputing/index.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const registerRoute = require('./Routes/RegisterRoute');
+const logInRoute = require('./Routes/LoginRoute');
+const https = require('https');
+
+//To Connect to DB
+mongoose.connect('mongodb://127.0.0.1:27017/CC_Aufgabe1', () => {
+  console.log("DB is connected");
+});
+
+//Routes
+app.use(bodyParser.json());
+app.use('/register', registerRoute);
+app.use('/logIn', logInRoute);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/Frontend/login_register.html');
+});
+
+app.get('/register', (req,res) => {
+  res.sendFile(__dirname + '/Frontend/register.html');
+});
+
+app.get('/index', (req,res) => {
+  res.sendFile(__dirname + '/Frontend/index.html');
+});
+
+app.get('/logIn', (req,res) => {
+  res.sendFile(__dirname + '/Frontend/logIn.html');
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/Frontend/register.html');
+});
+
+app.get('/chatroom', (req, res) => {
+  res.sendFile(__dirname + '/Frontend/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+//Socket ist Verbindung "on" heisst man will etwas fangen. Wenn sendFile Event eintritt wird Socket aktiviert
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+});
+
+/**
+ * Catching Events from Frontend here and send the payloads back to every Client
+ *
+ * With io.emit, the Payload gets back to the Chatroom so every Client can see the incoming Message. It is possible to den multi-media Files.
+ * There are different Events for different File Types so the Frontend Code can handle them.
+ */
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+    socket.on("sendImage", (imageUrl) => {
+      console.log("Image blob: " + imageUrl);
+      io.emit("sendImage", imageUrl);
+    });
+    socket.on("sendAudio", (audioUrl) => {
+      console.log("Audio blob: " + audioUrl);
+      io.emit("sendAudio", audioUrl);
+    });
+    socket.on("sendVideo", (videoUrl) => {
+      console.log("Video blob: " + videoUrl);
+      io.emit("sendVideo", videoUrl);
+
+    })
+});
+
+
+//http Server
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+//https Server
+/* const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'tls/ssl', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'tls/ssl', 'cert.pem')),
+  },
+  app
+)
+
+sslServer.listen(3000, () => console.log('Server on Port 3000')); */
